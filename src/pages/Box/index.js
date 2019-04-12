5fa972cc831c9679d80e6016a46deff7d810078e
jailsonsf <jailsonsoares002@gmail.com>
2019-04-12 20:50:23 -0300

    Update pages :rocket:

--- src/pages/Box/index.js
+++ src/pages/Box/index.js
@@ -1,11 +1,58 @@
 import React, { Component } from 'react';
+import { View, Text, FlatList, TouchableOpacity } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 
-import { View } from 'react-native';
+import api from '../../services/api';
+import Icon from 'react-native-vector-icons';
 
-// import { Container } from './styles';
+import { distanceInWords } from 'date-fns';
+import pt from 'date-fns/locale/pt';
+
+import styles from './styles';
 
 export default class Box extends Component {
+  state = { box: {} }
+
+  async componentDidMount() {
+    // this.subscribeToNewFiles();
+
+    const box = await AsyncStorage.getItem('@RocketBox:box');
+    const response = await api.get(`boxes/${box}`);
+
+    this.setState({ box: response.data });
+  };
+
+  renderItem = ({ item }) => (
+    <TouchableOpacity
+      onPress={() => {}}
+      style={styles.file}
+    >
+      <View style={styles.fileInfo}>
+        <Icon name="insert-drive-file" size={24} color="#a5cff"/>
+      </View>
+
+      <Text style={styles.fileDate}>
+        há{" "}
+        {distanceInWords(item.createdAt, new Date(), {
+          locale: pt
+        })}
+      </Text>
+    </TouchableOpacity>
+  );
+
   render() {
-    return <View />;
+    return (
+      <View style={styles.container}>
+        <Text style={styles.boxTitle}>{this.state.box.title}</Text>
+
+        <FlatList
+          styles={styles.list}
+          data={this.state.box.files}
+          keyExtractor={file => file._id}
+          ItemSeparatorComponent={() => <View style={styles.separator} />}
+          renderItem={this.renderItem}
+        />
+      </View>
+    );
   }
 }
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { View, Text, Image, TextInput, TouchableOpacity } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
+
 
 import api from '../../services/api';
 
@@ -9,13 +11,23 @@ import logo from '../../assets/logo.png';
 
 export default class Main extends Component {
   state = {
+    newBox: ''
+  };
 
+  async componentDidMount() {
+    const box = await AsyncStorage.getItem('@RocketBox:box');
+
+    if(box) {
+      this.props.navigation.navigate('Box');
+    }
   };
 
   handleSignIn = async () => {
     const response = await api.post('boxes', {
       title: this.state.newBox,
     });
+
+    await AsyncStorage.setItem('@RocketBox:box', response.data._id);
 
     this.props.navigation.navigate('Box');
   };
